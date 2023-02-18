3e40a8c1a0a2b8d15f97515dde6f50f047a6194e
john <john@funmula.com>
2023-02-18 11:50:50 +0800

    feat: add guest client

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,1 +1,9 @@
+import { HttpClient } from "../clients/http"
+
 export * as JWT from "./jwt"
+
+export class GuestClient extends HttpClient {
+  constructor() {
+    super(process.env.API_DOMAIN!)
+  }
+}
