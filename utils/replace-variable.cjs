f8d28f13047cb1dfd2d4e5a4674bff079729bd84
john <john@funmula.com>
2023-02-21 16:45:40 +0800

    chore: fix env replacer

--- utils/replace-variable.cjs
+++ utils/replace-variable.cjs
@@ -1,6 +1,10 @@
 const replace = require("replace-in-file")
 
 Object.keys(process.env).forEach(name => {
+  if (name !== "API_AUTH_DOMAIN" && name !== "API_DOMAIN") {
+    return
+  }
+
   replace({
     files: ["lib/**/*.js"],
     from: new RegExp(`process\.env\.${name}`, "g"),
