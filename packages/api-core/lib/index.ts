447d38dff0c1175fea3ce6059334115356b5a60f
john <john@funmula.com>
2023-03-02 14:34:53 +0800

    feat: api-core export enum

--- packages/api-core/lib/index.ts
+++ packages/api-core/lib/index.ts
@@ -2,3 +2,6 @@ export * as http from "./http"
 export * as error from "./error"
 export * as api from "./api"
 export * as auth from "./auth"
+
+// enum
+export { VideoVisibility, VideoProcessStatus } from "./api/video/types"
